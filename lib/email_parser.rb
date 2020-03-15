7d4fc2ef35348292ca235efc82d58af733d6aefc
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-15 15:13:02 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAddressParser
+class EmailParser
 
   def parse
     array = @EmailAddressParser.split(" " || ",")
