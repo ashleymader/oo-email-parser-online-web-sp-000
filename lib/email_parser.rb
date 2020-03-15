6be0f61fac21dd1bdf0ce362ef86a98c26bb08e9
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-15 16:13:25 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailAddressParser
     #if email == email in array, ignore the email
     #split email at , or whitespace 
     #return array
-    @email.split(/[,\s]+|[,\s]+g/)emails = emails.delete_if {|unique| unique == @email }
+    @email.split(/[,\s]+|[,\s]+g/).delete_if {|unique| unique == @email }
     
     end
 
